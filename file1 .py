88ad344442b49cf9485ed8d3b1417c00131d63c4
chinnugowda <35676783+chinnugowda@users.noreply.github.com>
2018-01-22 10:31:16 +0530

    Add files via upload

---
+++ file1 .py
@@ -0,0 +1,22 @@
+print("sahana")
+print("chinnu")
+#a=10
+#b=20
+#c=a+b
+#print(c)
+#a=25
+#b=5
+#c=a-b
+#print("C")
+#a=int input("enter a value")
+#b=int input("enter b value")
+#c=a+b
+#print(c)
+#a=int(input("enter a value"))
+#b=int(input("enter b value"))
+#c=a+b
+#print(c)
+a=float(input("enter a value"))
+b=float(input("enter b value"))
+c=a+b
+print(c)
