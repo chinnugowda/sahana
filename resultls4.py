aa8d246cb4ac008bd12ffac71e1257fafccdc36e
chinnugowda <35676783+chinnugowda@users.noreply.github.com>
2018-01-31 10:31:28 +0530

    Add files via upload

---
+++ resultls4.py
@@ -0,0 +1,14 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 2==2
+True
+>>> 2!=2
+False
+>>> (2+3)*4+8-6**2
+-8
+>>> 
+=============== RESTART: /home/cs2017a213/sahana/nestedls4.py ===============
+3
+7
+>>> 
