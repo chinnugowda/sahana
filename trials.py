8225a74ed49efa2e45029c04788a02faa5d4a582
chinnugowda <35676783+chinnugowda@users.noreply.github.com>
2018-02-19 09:31:00 +0530

    Add files via upload

---
+++ trials.py
@@ -0,0 +1,8 @@
+for i in "python":
+    if(i=='t'):
+        break
+    print(i)
+for i in "sahana":
+    if(i=='n'):
+       continue
+    print(i)
---
+++ trials_results.py
@@ -0,0 +1,17 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/cs2017a213/sahana/trials.py =================
+p
+y
+>>> 
+================= RESTART: /home/cs2017a213/sahana/trials.py =================
+p
+y
+s
+a
+h
+a
+a
+>>> 
